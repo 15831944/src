c3ba4684957620a1de6f86dbc7a986622de4ba9d
chrishuppor <chrishuppor@163.com>
2019-05-08 10:54:26 +0800

    Update UpdatePostTime.py

--- MyLittleTools/bin/UpdatePostTime.py
+++ MyLittleTools/bin/UpdatePostTime.py
@@ -6,9 +6,11 @@ import os
 
 #1.
 def SetAMTimeToCTime(file):
-    t = time.gmtime(os.path.getctime(file))
     os.utime(file, (os.path.getctime(file),os.path.getctime(file)))
-
+    
+def SetCTimeToMTime(file):
+    os.utime(file, (os.path.getmtime(file),os.path.getmtime(file)))
+    
 def ChangeFilesMTime2CTime(dir_path):
     for root, dirs, files in os.walk(dir_path):
         for item in files:
@@ -54,21 +56,28 @@ def ChangeFileNameStr(file_path, time_str):
     if new_path != file_path:
         os.rename(file_path,new_path)
     print("[+]" + os.path.basename(file_path) + "  NAME CHANGED.")
+    return new_path
         
 
 
-def ChnageAllFilesTimeStr(dir_path):    
+def ChnageAllFilesTimeStr(dir_path, update_list):    
     for root, dirs, files in os.walk(dir_path):
         for item in files:
             file_path = os.path.join(root, item)
-            time_str = GetMTimeStr(file_path);
-            ChangeFileTimeTitle(file_path, time_str)
-            ChangeFileNameStr(file_path, time_str)
+            for update_file in update_list:
+                if update_file in item:
+                    SetCTimeToMTime(file_path)
+                    time_str = GetMTimeStr(file_path);
+                    ChangeFileTimeTitle(file_path, time_str)
+                    new_path = ChangeFileNameStr(file_path, time_str)
+            if os.path.exists(file_path):
+                SetAMTimeToCTime(file_path)
 
 
 #1. 将所有文件的修改时间修改为创建时间       
 dir_path = "F:\\22. LIFE GAME\\myblog\\chrishuppor.github.io\\_posts"
+update_list = ["RE工具整理", "FileInstruction", "python2.7学习笔记", "IDAPython学习笔记"]
 #ChangeFilesMTime2CTime(dir_path);
 #2.将文件的名称和front中的时间字符串更新为文件的更新时间（如果更新时间与时间字符串一致说明文件没有更新过，不再处理）
-ChnageAllFilesTimeStr(dir_path)
+ChnageAllFilesTimeStr(dir_path, update_list)
 print("[*]exit.")
