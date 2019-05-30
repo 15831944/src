6f9f14ed486d9a9c4605e2a049d553a5e71d0e83
chrishuppor <chrishuppor@163.com>
2019-05-30 22:47:21 +0800

    $commitMes

---
+++ 专用小工具/git_push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $1 = 'blog' ]];
+then
+	cd "F:\\22. LIFE GAME\\myblog\\chrishuppor.github.io"
+elif [[ $1 = 'src' ]];
+then
+	cd "F:\\22. LIFE GAME\\myblog\\src"
+else
+	echo "usage: <repotype-'blog' or 'src'> (<commit>)"
+	exit
+fi
+
+commitMes = 'update'
+if [[ $2 != '' ]];
+then
+	$commitMes = $2
+fi
+
+git add .
+git commit -m $commitMes
+git push origin master
