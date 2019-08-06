3a9761b972407d5903539237bd55417eba9044ab
chrishuppor <chrishuppor@163.com>
2019-08-06 14:35:02 +0800

    update

--- 专用小工具/git_push.sh
+++ 专用小工具/git_push.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
 
+function PushTenTimes()
+{
+	cd "F:\\22. LIFE GAME\\myblog\\chrishuppor.github.io"
+	for ((i=1;i<=10;i++))
+	do
+		fullpath="F:\\22. LIFE GAME\\myblog\\chrishuppor.github.io\\_drafts\\helloworld.txt"
+		echo $i >> "$fullpath"
+		git add .
+		git commit -m "update"
+		git push origin master
+	done
+
+	rm "$fullpath"
+}
+
+function PushRepo()
+{
+	#commit
+	if [[ $1 != '' ]]
+	then
+		commitMes=$2
+	else
+		commitMes='update'
+	fi
+
+	git add .
+	git commit -m $commitMes
+	git push origin master
+}
+
+
+#repo
 if [[ $1 = 'blog' ]];
 then
 	cd "F:\\22. LIFE GAME\\myblog\\chrishuppor.github.io"
+	PushRepo $2
 elif [[ $1 = 'src' ]];
 then
 	cd "F:\\22. LIFE GAME\\myblog\\src"
-else
-	echo "usage: <repotype-'blog' or 'src'> (<commit>)"
-	exit
-fi
+	PushRepo $2
 
-commitMes='update'
-if [[ $2 != '' ]];
+#only push
+elif [[ $1 = 'OnlyPush' ]];
 then
-	$commitMes = $2
-fi
+	PushTenTimes
 
-git add .
-git commit -m '$commitMes'
-git push origin master
+#usage
+else
+	echo "usage1 to push local repo: <repotype-'blog' or 'src'> (<commit>)"
+	echo "usage2 to contribute 10 times: <'OnlyPush'>"
+fi
+exit
